4ced02e8fe49409fbb1435d8b8ac8a8419f19972
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-04 18:19:14 +0200

    Install express in index, create route files

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const messages = [
+  {
+    text: "Wagwan, mandem!",
+    user: "Valera",
+    added: new Date(),
+  },
+  {
+    text: "Zdarova zaebal!",
+    user: "Andrey",
+    added: new Date(),
+  },
+];
+
+app.route();
