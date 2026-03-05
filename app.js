46624ab9d2888d1cc9da7267e249e91a9de160a3
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-05 22:27:12 +0200

    Add comments for educational purposes

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 
+// Middleware that parses form data sent from HTML forms
 app.use(express.urlencoded({ extended: true }));
 
 const index = require("./routes/index");
 
+// Define where all routes start
 app.use("/", index);
 
-// Set paths
+// Tell Express where the view templates are stored
 app.set("views", path.join(__dirname, "views"));
+// Tell Express to use EJS as the template engine
 app.set("view engine", "ejs");
 
-// Run server
+// Define port server runs on
 const PORT = 3000;
 app.listen(PORT, (error) => {
   if (error) {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
+// Import Router function
 const { Router } = require("express");
+// New router instance
 const index = Router();
 
+// Messages data
 const messages = [
   {
     text: "Wagwan, mandem!",
@@ -14,22 +17,33 @@ const messages = [
   },
 ];
 
+// GET "/" route
 index.get("/", (req, res) => {
+
+  // Render index.ejs template
   res.render("index", { messages: messages });
 });
 
+// GET "/new" route
 index.get("/new", (req, res) => {
+
+  // Render form.ejs template
   res.render("form");
 });
 
+// POST "/new" route
+// Runs when form is submitted
 index.post("/new", (req, res) => {
+  
+  // Get form data from req.body
+  // Names match the "name" attributes in form inputs
   const text = req.body.messageText;
   const user = req.body.messageUser;
   const added = req.body.added;
 
-  console.log(req.body);
-
+  // Add new message object to messages array
   messages.push({ text, user, added });
+  // Redirect the user back to the homepage
   res.redirect("/");
 });
 
